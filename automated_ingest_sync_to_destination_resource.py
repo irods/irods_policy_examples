7063cbda559d900ff298851b4cdb573aa93ffdbb
Terrell Russell <terrellrussell@gmail.com>
2018-07-03 12:59:52 -0400

    reordering and comments

--- automated_ingest_sync_to_destination_resource.py
+++ automated_ingest_sync_to_destination_resource.py
@@ -20,7 +20,6 @@ SCANNED_RESOURCE = 'example_scanned_resc'
 DESTINATION_RESOURCE_ROOT = 'example_dest_resc_root'
 LIST_OF_DESTINATION_RESOURCE_LEAVES = ['a','b','c']
 
-
 # Given a condInput from a policy enforcement point
 # Search for a given key and return the value
 def get_value_for_key(kvpairs, key):
@@ -30,7 +29,7 @@ def get_value_for_key(kvpairs, key):
             return str(kvpairs.value[i])
     return "KEY NOT FOUND"
 
-# Given a replica number and a logical path
+# Given a logical path and a replica number
 # Perform a general query to determine the resource name
 def get_resource_name_by_replica_number(callback, logical_path, replica_number):
     coll_name = os.path.dirname(logical_path)
@@ -47,7 +46,7 @@ def get_resource_name_by_replica_number(callback, logical_path, replica_number):
     resource_name = str(genQueryOut.sqlResult[0].row(0)) if result_count > 0 else None
     return (result_count, resource_name)
 
-# Given a logical path and a resource name
+# Given a logical path
 # Perform a general query to determine the replica size
 def get_existing_replica_size_from_destination(callback, logical_path):
     coll_name = os.path.dirname(logical_path)
@@ -64,34 +63,6 @@ def get_existing_replica_size_from_destination(callback, logical_path):
 
     logical_size = int(genQueryOut.sqlResult[0].row(0)) if previously_registered_replicas > 0 else None
     return (previously_registered_replicas, logical_size)
-
-# Dynamic Policy Enforcement Point for rsModDataObjMeta
-# Required for Use Case 3
-def pep_api_mod_data_obj_meta_post(rule_args, callback, rei):
-    args = rule_args[2]
-    logical_path = str(args['logical_path'])
-
-    # do nothing if we're not on SCANNED_RESOURCE
-    target_replica_number = args['replica_number']
-    result_count, target_resource = get_resource_name_by_replica_number(callback, logical_path, target_replica_number)
-    if SCANNED_RESOURCE != target_resource:
-        return
-
-    # determine whether a replica already exists
-    previously_registered_replicas, logical_size = get_existing_replica_size_from_destination(callback, logical_path)
-    if previously_registered_replicas > 0:
-        # replica already exists, get the new replica's size
-        physical_size = int(args['dataSize'])
-        # compare sizes
-        if physical_size != logical_size:
-            # update if not equal
-            status = 0
-            params = "irodsAdmin=++++updateRepl=++++all="
-            callback.msiDataObjRepl(logical_path,params,status)
-        else:
-            # match, do nothing
-            # print("data sizes match - no action taken")
-            pass
 
 # Dynamic Policy Enforcement Point for rsPhyPathReg
 # Required for Use Cases 0, 1, 2
@@ -124,3 +95,31 @@ def pep_api_phy_path_reg_post(rule_args, callback, rei):
         status = 0
         params = "irodsAdmin=++++destRescName={0}".format(DESTINATION_RESOURCE_ROOT)
         callback.msiDataObjRepl(logical_path,params,status)
+
+# Dynamic Policy Enforcement Point for rsModDataObjMeta
+# Required for Use Case 3
+def pep_api_mod_data_obj_meta_post(rule_args, callback, rei):
+    args = rule_args[2]
+    logical_path = str(args['logical_path'])
+
+    # do nothing if we're not on SCANNED_RESOURCE
+    target_replica_number = args['replica_number']
+    result_count, target_resource = get_resource_name_by_replica_number(callback, logical_path, target_replica_number)
+    if SCANNED_RESOURCE != target_resource:
+        return
+
+    # determine whether a replica already exists
+    previously_registered_replicas, logical_size = get_existing_replica_size_from_destination(callback, logical_path)
+    if previously_registered_replicas > 0:
+        # replica already exists, get the new replica's size
+        physical_size = int(args['dataSize'])
+        # compare sizes
+        if physical_size != logical_size:
+            # update if not equal
+            status = 0
+            params = "irodsAdmin=++++updateRepl=++++all="
+            callback.msiDataObjRepl(logical_path,params,status)
+        else:
+            # match, do nothing
+            # print("data sizes match - no action taken")
+            pass
