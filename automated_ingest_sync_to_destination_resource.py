aaca106ff8dff467196f10f6de58e3ce1e65824e
Jason Coposky <jasonc@renci.org>
2018-08-30 12:38:38 -0400

    Updates for new Celery based queue

--- automated_ingest_sync_to_destination_resource.py
+++ automated_ingest_sync_to_destination_resource.py
@@ -15,10 +15,42 @@ import os
 # 3 - Local modifications are made to existing replicas on SCANNED_RESOURCE
 #     Detect and update all the other replicas
 
+
+
+###################################
 # POLICY CONFIGURATION VARIABLES
-SCANNED_RESOURCE = 'example_scanned_resc'
-DESTINATION_RESOURCE_ROOT = 'example_dest_resc_root'
-LIST_OF_DESTINATION_RESOURCE_LEAVES = ['a','b','c']
+
+SCANNED_RESOURCE_ROOT = 'pt'#'example_scanned_resc'
+
+DESTINATION_RESOURCE_ROOT = 'dst_resc'#'example_dest_resc_root'
+
+# By default, for resources which have no children, these are set to be the above variables
+# but for resources which are roots of a hierarchy, these variables must be set to contain
+# all the leaves of the given scanned or destination resource hierarchies.  For example,
+# given an ilsresc output of:
+#
+# src_root:passthru
+# └── src_random:random
+#     ├── src0:unixfilesystem
+#     ├── src1:unixfilesystem
+#     └── src2:unixfilesystem
+#
+# LIST_OF_SCANNED_RESOURCE_LEAVES  = ['src0', 'src1', 'src2']
+#
+# dst_root:passthru
+# └── dst_compound:compound
+#     ├── dst_cache:unixfilesystem
+#     └── dst_archive:s3
+#
+# LIST_OF_DESTINATION_RESOURCE_LEAVES = ['dst_cache', 'dst_archive']
+
+LIST_OF_SCANNED_RESOURCE_LEAVES = ['ufs0']
+LIST_OF_DESTINATION_RESOURCE_LEAVES = ['dst_resc']
+
+###################################
+
+def resource_is_not_target(resc_name):
+    return not ((resc_name == SCANNED_RESOURCE_ROOT) or (resc_name in LIST_OF_SCANNED_RESOURCE_LEAVES))
 
 # Given a condInput from a policy enforcement point
 # Search for a given key and return the value
@@ -29,7 +61,7 @@ def get_value_for_key(kvpairs, key):
             return str(kvpairs.value[i])
     return "KEY NOT FOUND"
 
-# Given a logical path and a replica number
+# Given a replica number and a logical path
 # Perform a general query to determine the resource name
 def get_resource_name_by_replica_number(callback, logical_path, replica_number):
     coll_name = os.path.dirname(logical_path)
@@ -46,13 +78,13 @@ def get_resource_name_by_replica_number(callback, logical_path, replica_number):
     resource_name = str(genQueryOut.sqlResult[0].row(0)) if result_count > 0 else None
     return (result_count, resource_name)
 
-# Given a logical path
+# Given a logical path and a resource name
 # Perform a general query to determine the replica size
 def get_existing_replica_size_from_destination(callback, logical_path):
     coll_name = os.path.dirname(logical_path)
     data_name = logical_path.split('/')[-1]
     resc_names = ','.join(["'"+i+"'" for i in LIST_OF_DESTINATION_RESOURCE_LEAVES])
-    conditions = "COLL_NAME = '{0}' AND DATA_NAME = '{1}' AND RESC_NAME IN ({2})".format(coll_name, data_name, resc_names)
+    conditions = "COLL_NAME = '{0}' AND DATA_NAME = '{1}' AND RESC_NAME in ({2})".format(coll_name, data_name, resc_names)
 
     ret_val = callback.msiMakeGenQuery("DATA_SIZE", conditions, irods_types.GenQueryInp())
     genQueryInp = ret_val['arguments'][2]
@@ -64,38 +96,6 @@ def get_existing_replica_size_from_destination(callback, logical_path):
     logical_size = int(genQueryOut.sqlResult[0].row(0)) if previously_registered_replicas > 0 else None
     return (previously_registered_replicas, logical_size)
 
-# Dynamic Policy Enforcement Point for rsPhyPathReg
-# Required for Use Cases 0, 1, 2
-def pep_api_phy_path_reg_post(rule_args, callback, rei):
-    args = rule_args[2]
-    logical_path = str(args.objPath)
-
-    # do nothing if we're not on SCANNED_RESOURCE
-    target_resource = get_value_for_key(args.condInput,"destRescName")
-    if SCANNED_RESOURCE != target_resource:
-        return
-
-    # determine whether a replica already exists
-    previously_registered_replicas, logical_size = get_existing_replica_size_from_destination(callback, logical_path)
-    if previously_registered_replicas > 0:
-        # replica already exists, stat the file system to get the new replica's size
-        physical_path = get_value_for_key(args.condInput,"filePath")
-        physical_size = os.stat(physical_path).st_size
-        # compare new replica size with existing replica size
-        if physical_size != logical_size:
-            # Use Case 2 - Sizes Do Not Match, Update All Existing Replicas
-            status = 0
-            params = "irodsAdmin=++++updateRepl=++++all="
-            callback.msiDataObjRepl(logical_path,params,status)
-        else:
-            # Use Case 1 - Sizes Match, Do Nothing
-            pass
-    else:
-        # Use Case 0 - Replicate New Data to DESTINATION_RESOURCE_ROOT
-        status = 0
-        params = "irodsAdmin=++++destRescName={0}".format(DESTINATION_RESOURCE_ROOT)
-        callback.msiDataObjRepl(logical_path,params,status)
-
 # Dynamic Policy Enforcement Point for rsModDataObjMeta
 # Required for Use Case 3
 def pep_api_mod_data_obj_meta_post(rule_args, callback, rei):
@@ -105,7 +105,10 @@ def pep_api_mod_data_obj_meta_post(rule_args, callback, rei):
     # do nothing if we're not on SCANNED_RESOURCE
     target_replica_number = args['replica_number']
     result_count, target_resource = get_resource_name_by_replica_number(callback, logical_path, target_replica_number)
-    if SCANNED_RESOURCE != target_resource:
+
+
+    if resource_is_not_target(target_resource):
+        #print("pep_api_mod_metadata_post - resources are not a match")
         return
 
     # determine whether a replica already exists
@@ -115,11 +118,56 @@ def pep_api_mod_data_obj_meta_post(rule_args, callback, rei):
         physical_size = int(args['dataSize'])
         # compare sizes
         if physical_size != logical_size:
+            #print("pep_api_mod_data_obj_meta - Use Case 3")
             # update if not equal
             status = 0
             params = "irodsAdmin=++++updateRepl=++++all="
             callback.msiDataObjRepl(logical_path,params,status)
         else:
             # match, do nothing
-            # print("data sizes match - no action taken")
+            #print("pep_api_mod_metadata_post - data sizes match, no action taken")
             pass
+
+# Dynamic Policy Enforcement Point for rsPhyPathReg
+# Required for Use Cases 0, 1, 2
+def pep_api_phy_path_reg_post(rule_args, callback, rei):
+    args = rule_args[2]
+    logical_path = str(args.objPath)
+    resc_hier = get_value_for_key(args.condInput,"resc_hier")
+
+    if 'KEY NOT FOUND' != resc_hier:
+        target_resource = resc_hier.split(';')[0]
+    else:
+        target_resource = get_value_for_key(args.condInput,"destRescName")
+
+    # do nothing if we're not on SCANNED_RESOURCE
+    if resource_is_not_target(target_resource):
+        #print("pep_api_phy_path_reg_post - resources are not a match")
+        return
+
+
+    # determine whether a replica already exists
+    previously_registered_replicas, logical_size = get_existing_replica_size_from_destination(callback, logical_path)
+    if previously_registered_replicas > 0:
+        # replica already exists, stat the file system to get the new replica's size
+        physical_path = get_value_for_key(args.condInput,"filePath")
+
+        data_size_str = get_value_for_key(args.condInput,"dataSize")
+        physical_size = (int(data_size_str) if data_size_str != 'KEY NOT FOUND' else os.stat(physical_path).st_size)
+        # compare new replica size with existing replica size
+        if physical_size != logical_size:
+            # Use Case 2 - Sizes Do Not Match, Update All Existing Replicas
+            #print("pep_api_phy_path_reg_post - Use Case 2")
+            status = 0
+            params = "irodsAdmin=++++updateRepl=++++all="
+            callback.msiDataObjRepl(logical_path,params,status)
+        else:
+            # Use Case 1 - Sizes Match, Do Nothing
+            #print("pep_api_phy_path_reg_post - Use Case 1")
+            pass
+    else:
+        # Use Case 0 - Replicate New Data to DESTINATION_RESOURCE_ROOT
+        #print("pep_api_phy_path_reg_post - Use Case 0")
+        status = 0
+        params = "irodsAdmin=++++destRescName={0}".format(DESTINATION_RESOURCE_ROOT)
+        callback.msiDataObjRepl(logical_path,params,status)
